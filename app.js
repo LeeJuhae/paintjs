8ab8811492f47fc92880fd80c59ed31d23de86be
LeeJuhae <tlatmsdlwngo@naver.com>
2020-05-19 17:59:33 +0900

    MODIFIED: Can Change Color, Brush size, filling mode

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
+const INITIAL_COLOR = "#2c2c2c";
 // For manipulating pixels, Set up width and height.
 // This must be consistent with CSS Canvas Size.
 canvas.width = 600;
 canvas.height = 600;
 
-ctx.strokeStyle = "#2c2c2c" // Line color
+ctx.strokeStyle = INITIAL_COLOR; // Line color
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
 	painting = false;
@@ -31,21 +37,49 @@ function onMouseMove(event){
 	}
 }
 
-
-function onMouseDown(event){
-	painting = true;
+function handleColorClick(event){
+	const color = event.target.style.backgroundColor;
+	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
 }
 
-function onMouseUp(event){
-	stopPainting();
+function handleRangeChange(event){
+	const size = event.target.value;
+	ctx.lineWidth = size;
 }
 
+function handleModeClick(){
+	if (filling === true){
+		filling = false;
+		mode.innerText = "fill";
+	} else{
+		filling = true;
+		mode.innerText = "paint";
+	}
+}
+
+function handleCanvasClick(){
+	if (filling){
+		ctx.fillRect(0,0, canvas.width, canvas.height);
+	}
+}
 
 if (canvas){
 	canvas.addEventListener("mousemove", onMouseMove);
 	canvas.addEventListener("mousedown", startPainting);
-	// canvas.addEventListener("mousedown", onMouseDown);
 	canvas.addEventListener("mouseup", stopPainting);
-	// canvas.addEventListener("mouseup", onMouseUp);
 	canvas.addEventListener("mouseleave", stopPainting);
+	canvas.addEventListener("click", handleCanvasClick);
+}
+
+// Array.from() : object -> array
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+// Array.from(colors).forEach(potato => potato.addEventListener("click", handleColorClick));
+
+if (range){
+	range.addEventListener("input", handleRangeChange); //"input"
+}
+
+if (mode){
+	mode.addEventListener("click", handleModeClick);
 }
